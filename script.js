b004932cce0e4012e7e0906da748e4fa2ee8ef35
Soumya Prasad Chandra <132330925+Soumya2024@users.noreply.github.com>
2026-01-11 20:41:40 +0530

    changes code and changes skills added

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 $(document).ready(function () {
-  // Scroll down sticky navbar script start
-  //this is javascript
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
-    // Scroll down sticky navbar script end
-
-    // Scroll up Button script start
     if (this.scrollY > 500) {
       $(".scroll-up-btn").addClass("show");
     } else {
       $(".scroll-up-btn").removeClass("show");
     }
-    // Scroll up Button script end
-
-    // Fade In & Fade Out Elements on Scroll script start
     $(".fadein").each(function (i) {
       var bottom_of_element = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -28,17 +20,12 @@ $(document).ready(function () {
       if (bottom_of_window < bottom_of_element) {
         $(this).removeClass("showme");
       }
-      // Fade In & Fade Out Elements on Scroll script end
     });
   });
 
-  // Scroll up Button script start
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
   });
-  // Scroll up Button script end
-
-  // Typing animation script start
   var typed = new Typed(".typing", {
     strings: ["Post Graduate", "Software Engineer", "Programmer"],
     typeSpeed: 120,
@@ -53,14 +40,11 @@ $(document).ready(function () {
     loop: true,
   });
 
-  // toggle menu/navbar script start
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
-  // toggle menu/navbar script end
 
-  // owl carousel script start
   $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
@@ -81,5 +65,19 @@ $(document).ready(function () {
       },
     },
   });
-  // owl carousel script end
+
+  $(".read-more-btn").click(function () {
+    const moreText = $(".more-text");
+    const dots = $(".dots");
+
+    if (moreText.is(":visible")) {
+      moreText.hide();
+      dots.show();
+      $(this).html('Read more <i class="fas fa-angle-double-right"></i>');
+    } else {
+      moreText.show();
+      dots.hide();
+      $(this).html('Read less <i class="fas fa-angle-double-up"></i>');
+    }
+  });
 });
